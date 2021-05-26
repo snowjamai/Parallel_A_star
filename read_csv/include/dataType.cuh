c663f8082b8d1810481e3113302ea190d78dd721
HyungJun Lee <rhtm02@gmail.com>
2021-05-26 12:04:51 +0900

    Add implement header datatype

---
+++ read_csv/include/dataType.cuh
@@ -0,0 +1,15 @@
+//
+// Created by hyungjun on 5/25/21.
+//
+
+#ifndef ASTAR_PAPER_DATATYPE_CUH
+#define ASTAR_PAPER_DATATYPE_CUH
+
+typedef struct Node {
+    int r_len = 0;
+    int dest[10];
+    float len[10];
+} Node;
+
+
+#endif //ASTAR_PAPER_DATATYPE_CUH
